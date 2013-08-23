236f474f457b964d20e65f79358a2f5a7135c1fc
Fara Ashiru <fara@crisply.com>
2013-08-23 10:15:55 -0400

    commit before moving macs

--- _/js/plugins.js
+++ _/js/plugins.js
@@ -22,3 +22,72 @@
 }());
 
 // Place any jQuery/helper plugins in here.
+
+$(document).ready(function() {
+  function filterPath(string) {
+  return string
+    .replace(/^\//,'')
+    .replace(/(index|default).[a-zA-Z]{3,4}$/,'')
+    .replace(/\/$/,'');
+  }
+  var locationPath = filterPath(location.pathname);
+  var scrollElem = scrollableElement('html', 'body');
+ 
+  $('a[href*=#]').each(function() {
+    var thisPath = filterPath(this.pathname) || locationPath;
+    if (  locationPath == thisPath
+    && (location.hostname == this.hostname || !this.hostname)
+    && this.hash.replace(/#/,'') ) {
+      var $target = $(this.hash), target = this.hash;
+      if (target) {
+        var targetOffset = $target.offset().top;
+        $(this).click(function(event) {
+          event.preventDefault();
+          $(scrollElem).animate({scrollTop: targetOffset}, 400, function() {
+            location.hash = target;
+          });
+        });
+      }
+    }
+  });
+ 
+  // use the first element that is "scrollable"
+  function scrollableElement(els) {
+    for (var i = 0, argLength = arguments.length; i <argLength; i++) {
+      var el = arguments[i],
+          $scrollElement = $(el);
+      if ($scrollElement.scrollTop()> 0) {
+        return el;
+      } else {
+        $scrollElement.scrollTop(1);
+        var isScrollable = $scrollElement.scrollTop()> 0;
+        $scrollElement.scrollTop(0);
+        if (isScrollable) {
+          return el;
+        }
+      }
+    }
+    return [];
+  }
+  
+  $(".day").click(function (evt){
+     var content = "." + evt.currentTarget.innerHTML.toLowerCase().replace(/ /g,'') + "content";
+     $(".black-overlay").show();
+
+     $(".title").show();
+     $(".title").innerHTML = evt.currentTarget.innerHTML;
+     $(content).show();
+  });
+
+  $(".arrow-down, .black-overlay, .x-close").click(function (evt){
+     $(".black-overlay").hide();
+     $(".title").hide();
+     $(".day-box").hide();
+     $(".day1content,.day2content,.day3content,.day4content,.day5content,.day6content,.day7content,.day8content ").hide();
+     console.log("evt: ", evt);
+  });
+
+
+});
+
+
